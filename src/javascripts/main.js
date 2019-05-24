3a4f4da863d7c8b8f6e3daf3e234aac539bb865e
Matt Bell <matthewaustinbell@gmail.com>
2019-05-24 18:01:26 -0500

    wip

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,3 +1,15 @@
+import eat from './components/health';
+import fight from './components/fight';
+import play from './components/play';
+import sleep from './components/sleep';
+
 import '../styles/main.scss';
 
-console.error('hi');
+const init = () => {
+  eat.domStringBuilder();
+  play.domStringBuilder();
+  fight.domStringBuilder();
+  sleep.domStringBuilder();
+};
+
+init();
